3aeea870a7ae51bc05211889b0618dfc078ffb28
NoobNobody <83828105+NoobNobody@users.noreply.github.com>
2022-01-12 21:14:03 +0100

    Add files via upload

---
+++ Laby5/lab5/Program.cs
@@ -0,0 +1,152 @@
+﻿using lab5;
+//zad1
+// do testowania odkomentowac wywołanie
+try
+{
+  //  napis(null);  
+}catch (NullReferenceException e)
+{
+    Console.WriteLine(e.StackTrace);
+    throw new Exception("nie moze byc null",e);
+
+}
+//Unhandled exception.System.NullReferenceException: Object reference not set to an instance of an object.
+
+
+int napis(string n)
+{
+    return n.Length;
+}
+
+
+
+//zad2
+
+// do testowania odkomentowac wywołanie
+//  zad2();
+
+
+void zad2()
+{
+    Random Rand = new Random();
+    switch (Rand.Next(1, 4))
+    {
+        case 1:
+            {
+                try
+                {
+      
+                throw new Exception();
+
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine("Wyjatek 1");
+                }
+                break;
+            }
+
+        case 2:
+            {
+                try
+                {
+                    throw new Exception();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Wyjatek 2");
+                }
+
+
+                break;
+            }
+        case 3:
+            {
+                try
+                {
+                    throw new Exception();
+                }
+                catch( Exception e)
+                {
+                    Console.WriteLine("Wyjatek 3");
+                }
+                break;
+            }
+
+    }
+}
+//zad3
+
+
+// do testowania odkomentowac wywołanie
+//zad3();
+void zad3()
+{
+    try
+    {
+        throw new Exception1();
+    }catch(Exception1 e)
+    {
+        throw new Exception2("wyjatek nowy");
+
+    }catch(Exception2 e)
+    {
+        Console.WriteLine("Mozna jak najbardziej");
+    }
+
+   
+
+
+
+}
+//zad 4
+// do testowania odkomentowac wywołanie
+//zad4();
+void zad4 (){
+    SomeClass someClassObj = new SomeClass();
+    int i = 1;
+    try
+    {
+
+        someClassObj.CanThrowException();
+        i++;
+        someClassObj.CanThrowException();
+        i++;
+        someClassObj.CanThrowException();
+        i++;
+        someClassObj.CanThrowException();
+        i++;
+        someClassObj.CanThrowException();
+        i++;
+        Console.WriteLine("Brak");
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine("wystapilo na " + i + " instrukcji");
+        Console.WriteLine(e.StackTrace);
+
+    }
+}
+//zad5 zad6
+zad5_6();
+void zad5_6()
+{
+    Student student1 = new Student("Adam", "Nowak", 117801);
+    Student student2 = new Student();
+
+    //moja metoda 
+    student1.kopiuj(student1, student2);
+
+    Console.WriteLine("Sklonowano do student2: "+student2.imie);
+    Console.WriteLine("Sklonowano do student2: "+student2.nazwisko);
+    Console.WriteLine("Sklonowano do student2: " +student2.album);
+
+    //MemberwiseClone()
+    Student student3 = student1.kopiujzad6();
+    Console.WriteLine("Student3 kopiowanie MemberwiseClone()");
+    Console.WriteLine(student3.imie);
+    Console.WriteLine(student3.nazwisko);
+    Console.WriteLine(student3.album);
+
+
+}
---
+++ Laby5/lab5/Student.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab5
+{
+    public class Student : ICloneable
+    {
+     public   string imie;
+     public   string nazwisko;
+     public   int album;
+
+        //zad6
+        public object Clone()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Student kopiujzad6()
+        {
+            try
+            {
+                return (Student)MemberwiseClone();
+            }catch (Exception e)
+            {
+                return null;
+            }
+
+
+        }
+
+        //zad5
+        public void kopiuj(Student student1, Student student2)
+        {
+
+                try
+                {
+                    student2.imie = student1.imie;
+                    student2.nazwisko = student1.nazwisko;
+                    student2.album = student1.album;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("nie moze byc null");
+                }
+            }
+        
+        public Student()
+        {
+
+        }
+        public Student(string imie, string nazwisko, int album)
+        {
+            this.imie = imie;
+            this.nazwisko = nazwisko;
+            this.album = album;
+        }
+    }
+}
